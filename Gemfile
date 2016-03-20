e215ac1ae97a884bb92da3047bca894a89ae01e7
tranhuyhoangbka <hoangth92.nd@gmail.com>
2016-03-21 00:38:59 +0700

    Adds update action the user controller

--- Gemfile
+++ Gemfile
@@ -11,14 +11,21 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.1.0'
-# See https://github.com/rails/execjs#readme for more supported runtimes
+# See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'i18n'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
+gem 'turbolinks'
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-
+gem "devise"
+#gem "sabisu_rails"
+# api support
+gem "active_model_serializers"
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
@@ -28,28 +35,14 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-# authentication
-gem "devise"
-
-# api support
-gem "active_model_serializers"
-gem "sabisu_rails"
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'pry-rails'
   gem 'faker'
 end
 
-group :development do
-  # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
-
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
-end
-
 group :test do
   gem "rspec-rails"
   gem "factory_girl_rails"
-  gem "shoulda-matchers"
+  gem 'shoulda-matchers'
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -9,9 +9,63 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       get :show, id: @user.id, format: :json
     end
     it "returns the information about a reporter on a hash" do
-      binding.pry
       user_response = JSON.parse(response.body, symbolize_names: true)
       expect(user_response[:email]).to eql @user.email
+    end
+  end
+
+  describe "POST #create" do
+    context "when is successfully created" do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, {user: @user_attributes}, format: :json
+      end
+
+      it "renders the json representation for for the user record just created" do
+        user_response = JSON.parse response.body, symbolize_names: true
+        expect(user_response[:email]).to eq @user_attributes[:email]
+      end
+    end
+
+    context "when is not created" do
+      before do
+        @invalid_user_attributes = {password: "12345678", password_confirmation: "12345678"}
+        post :create, {user: @invalid_user_attributes}, format: :json
+      end
+      let(:user_response){JSON.parse response.body, symbolize_names: true}
+      it "renders an errors json" do
+        expect(user_response).to have_key(:errors)
+      end
+      it "renders the json errors on why the user could not be created" do
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+    end
+  end
+
+  describe "PUT/PATCH #update" do
+    context "when is successfully updated" do
+      before do
+        @user = FactoryGirl.create :user
+        patch :update, {id: @user.id, user: {email: "newmail@example.com"}}, format: :json
+      end
+      it "renders the json representation for the updated user" do
+        user_response = JSON.parse response.body, symbolize_names: true
+        expect(user_response[:email]).to eq "newmail@example.com"
+      end
+    end
+
+    context "renders an errors json" do
+      before do
+        @user = FactoryGirl.create :user
+        patch :update, {id: @user.id, user: {email: "babemail.com"}}, format: :json
+      end
+      let(:user_response){JSON.parse response.body, symbolize_names: true}
+      it "renders an errors json" do
+        expect(user_response).to have_key :errors
+      end
+      it "renders the json errors on why the user could not be created" do
+        expect(user_response[:errors][:email]).to include "is invalid"
+      end
     end
   end
 end
